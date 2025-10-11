ab8631251237c007f2426a26ab2d835d93207683
Shankar-105 <shankarbhavani862@gmail.com>
2025-10-11 16:48:43 +0530

    using fastpai's OAuth

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -3,12 +3,14 @@ from app import db,models,oauth2
 from sqlalchemy.orm import Session
 import app.utils as utils
 import app.schemas as sch
-
+from fastapi.security import OAuth2PasswordRequestForm
 
 router=APIRouter(tags=['Authentication'])
 @router.post("/login",status_code=status.HTTP_202_ACCEPTED)
 # method which log's in user if he has an account
-def loginUser(userCred:sch.UserLoginCred=Body(...),db:Session=Depends(db.getDb)):
+# using the built-in schema for login 'OAuth2PasswordRequestForm'
+# which is equivalent to our 'sch.UserLoginCred'
+def loginUser(userCred:OAuth2PasswordRequestForm=Depends(),db:Session=Depends(db.getDb)):
   # checks against the db for the username provided 
   isUserPresent=db.query(models.User).filter(models.User.username==userCred.username).first()
   # if not found tell the user not found

4c06fa791896a1b29afe525187e5837126391ece
Shankar-105 <shankarbhavani862@gmail.com>
2025-10-11 16:25:12 +0530

    added comments for better understanding

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,12 +1,27 @@
 from jose import JWTError,jwt
 from datetime import datetime,timedelta
+
+# basically for the generation of an JWT token it requries
+# 1. an ALGORITHM
+# 2. some user data like suppose here user id,username
+# and to this we add a newField called the expiry time
+# for that amount of time the jwt token will be valid
+# 3. a secret key
 ALGORITHM="HS256"
-SECERT_KEY="iota"
+SECRET_KEY="iota"
 EXPIRE_TIME=30
 
 def createAccessToken(data:dict):
+    # create a copy of the data becuase
+    # we create update or add a new field to it 
+    # so the original one doesnt change
     dataCopy=data.copy()
+    # create some expiry time 30 mins from now the token creation
     expireTime=datetime.now()+timedelta(minutes=EXPIRE_TIME)
+    # update the data with expiry time convert it to seconds
+    # using timestamp() mtd so that serialization to json is possible
     dataCopy.update({"expTime":int(expireTime.timestamp())})
-    jwtToken=jwt.encode(dataCopy,SECERT_KEY,algorithm=ALGORITHM)
+    # encode or create a jwt token
+    jwtToken=jwt.encode(dataCopy,SECRET_KEY,algorithm=ALGORITHM)
+    # return the token
     return jwtToken
--- app/routes/auth.py
+++ app/routes/auth.py
@@ -7,15 +7,20 @@ import app.schemas as sch
 
 router=APIRouter(tags=['Authentication'])
 @router.post("/login",status_code=status.HTTP_202_ACCEPTED)
+# method which log's in user if he has an account
 def loginUser(userCred:sch.UserLoginCred=Body(...),db:Session=Depends(db.getDb)):
+  # checks against the db for the username provided 
   isUserPresent=db.query(models.User).filter(models.User.username==userCred.username).first()
-
+  # if not found tell the user not found
   if not isUserPresent:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"user {userCred.username} not Found")
+  # if found but he entered a wrong password tell him 
   if not utils.verifyPassword(userCred.password,isUserPresent.password):
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"incorrect password")
-  
+  # if both username and password verfication is successfull call
+  # the createAccessToken from oauth2 file which generates an jwt token
   accessToken=oauth2.createAccessToken({"userId":isUserPresent.id,"userName":isUserPresent.username})
+  # return the access token
   return {"access_token":accessToken,"token_type":"bearer"}
 
  

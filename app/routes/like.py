47255c086c59e44ad63e5fa5eb6941c061a771e3
Shankar-105 <shankarbhavani862@gmail.com>
2025-10-17 17:40:46 +0530

    added comments for understanding of vote system

--- app/routes/like.py
+++ app/routes/like.py
@@ -10,17 +10,27 @@ router=APIRouter(
 )
 
 @router.post("/vote",status_code=status.HTTP_201_CREATED)
+# get the post user that user wants to vote on with which user he is
 def vote(post:sch.VoteModel=Body(...),db:Session=Depends(getDb),currentUser:sch.TokenModel=Depends(oauth2.getCurrentUser)):
+    # search for the post he wants to vote on against the db 
+    # to firstly check whether that particular post is present or not in the db
     queriedPost=db.query(models.Post).filter(models.Post.id==post.postId).first()
+    # if not present just raise an 404 error
     if not queriedPost:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post with Id {post.postId} not Found")
+    # if present in db then search in the votes table for knowing if he has
+    # already voted on the post or not
     currentVote=db.query(models.Likes).filter(and_(models.Likes.postId==post.postId,models.Likes.userId==currentUser.id)).first()
     try:
+        # if currentVote is not None then record of voting exists
+        # by that particular user in the votes table
         if currentVote:
-            # User already voted, handle toggle or removal
+            # User already voted, with the same choice
             if currentVote.action == post.choice:
                 # Same choice again means remove the vote
                 db.delete(currentVote)
+                # update the same on the posts table also
+                # by removing the vote accordingly (like/dislike)
                 if post.choice:
                     queriedPost.likes -= 1
                 else:
@@ -48,13 +58,16 @@ def vote(post:sch.VoteModel=Body(...),db:Session=Depends(getDb),currentUser:sch.
                 action=post.choice
             )
             db.add(newVote)
+            # if user choice is true increase likes count
             if post.choice:
                 queriedPost.likes += 1
+            # or else dilikes count
             else:
                 queriedPost.disLikes += 1
             db.commit()
             db.refresh(queriedPost)
             return {"message": "New vote added successfully"}
+    # triggers if any thing goes wrong in db as the logic is solid
     except IntegrityError:
         db.rollback()
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Database error, please try again")

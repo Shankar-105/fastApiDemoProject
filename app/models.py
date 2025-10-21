0da8dfca870a80579a016aba99bb3afa9e339a85
Shankar-105 <shankarbhavani862@gmail.com>
2025-10-21 12:40:20 +0530

    new attibutes to the users table addded

--- app/models.py
+++ app/models.py
@@ -20,7 +20,10 @@ class User(Base):
       __tablename__='users'
       id=Column(Integer,primary_key=True,nullable=False)
       username=Column(String,nullable=False,unique=True)
-      password=Column(String,nullable=False)
+      hashed_password=Column(String,nullable=False)
+      email=Column(String,nullable=False)
+      bio=Column(String,nullable=True)
+      profile_picture=Column(String,nullable=True)
       created_at=Column(TIMESTAMP(timezone=True),nullable=False,server_default=text('now()'))
 
 class Votes(Base):

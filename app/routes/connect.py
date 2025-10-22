411f87238cf8cf4d1ab833ca1bff784c8cc35671
Shankar-105 <shankarbhavani862@gmail.com>
2025-10-22 17:28:09 +0530

    now users can follow each other

--- app/routes/connect.py
+++ app/routes/connect.py
@@ -6,9 +6,22 @@ from sqlalchemy.orm import Session
 from sqlalchemy import and_
 from sqlalchemy.exc import IntegrityError
 
-router=APIRouter(tags='connections')
+router=APIRouter(tags=['connections'])
 
 @router.post("/connect/{user_id}",status_code=status.HTTP_201_CREATED)
 def follow(user_id:int,db:Session=Depends(getDb),currentUser:models.User=Depends(oauth2.getCurrentUser)):
-    return None
+    userToFollow=db.query(models.User).filter(models.User.id==user_id).first()
+    if not userToFollow:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="User doesnt exist")
+    if userToFollow.id == currentUser.id:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Cannot follow yourself")
+    if userToFollow in currentUser.following:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Your already following this user")
+    try:
+        currentUser.following.append(userToFollow)
+        db.commit()
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Failed to follow user")
+    return {"message":f"followed user {userToFollow}"}
     

108d0dfcce7b6afbc9c5b481c46187ccd02c5de3
Shankar-105 <shankarbhavani862@gmail.com>
2025-10-22 17:35:56 +0530

    user can unfollow other users

--- app/routes/connect.py
+++ app/routes/connect.py
@@ -8,7 +8,7 @@ from sqlalchemy.exc import IntegrityError
 
 router=APIRouter(tags=['connections'])
 
-@router.post("/connect/{user_id}",status_code=status.HTTP_201_CREATED)
+@router.post("/follow/{user_id}",status_code=status.HTTP_201_CREATED)
 def follow(user_id:int,db:Session=Depends(getDb),currentUser:models.User=Depends(oauth2.getCurrentUser)):
     userToFollow=db.query(models.User).filter(models.User.id==user_id).first()
     if not userToFollow:
@@ -23,5 +23,19 @@ def follow(user_id:int,db:Session=Depends(getDb),currentUser:models.User=Depends
     except IntegrityError:
         db.rollback()
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Failed to follow user")
-    return {"message":f"followed user {userToFollow}"}
+    return {"message":f"followed user {userToFollow.username}"}
     
+@router.delete("/unfollow/{user_id}",status_code=status.HTTP_201_CREATED)
+def unfollow(user_id:int,db:Session=Depends(getDb),currentUser:models.User=Depends(oauth2.getCurrentUser)):
+    userToUnFollow=db.query(models.User).filter(models.User.id==user_id).first()
+    if not userToUnFollow:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="User doesnt exist")
+    if userToUnFollow not in currentUser.following:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Not following this user")
+    try:
+        currentUser.following.remove(userToUnFollow)
+        db.commit()
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Failed to follow user")
+    return {"message":f"UnFollowed user {userToUnFollow.username}"}

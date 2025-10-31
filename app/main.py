8dbbaa7baf7131dbdfb2adfa57a7b007209fd9b2
Shankar-105 <shankarbhavani862@gmail.com>
2025-10-31 11:14:20 +0530

    separated me from others

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@
 from fastapi import FastAPI
 from app import models
 from app.db import engine
-from app.routes import posts,users,auth,like,connect,comment,search
+from app.routes import posts,users,auth,like,connect,comment,search,me
 from fastapi.middleware.cors import CORSMiddleware
 models.Base.metadata.create_all(bind=engine)
 
@@ -25,3 +25,4 @@ app.include_router(like.router)
 app.include_router(connect.router)
 app.include_router(comment.router)
 app.include_router(search.router)
+app.include_router(me.router)
---
+++ app/routes/me.py
@@ -0,0 +1,154 @@
+from fastapi import status,HTTPException,Depends,Body,APIRouter,Form
+import app.schemas as sch
+from app import models,db,oauth2
+from sqlalchemy.orm import Session
+import os,shutil
+from fastapi import UploadFile,File
+from sqlalchemy import and_,distinct,func,case
+
+router=APIRouter(
+    tags=['me']
+)
+
+# a patch endpoint so that user can update what he wants to unlike put
+# profile picture cannot be taken as a json data so it must be passed via Form
+# and the username and bio can be passed via Body params but its resulting in an
+# ambiguity as one of the section is being passed via Form and the other via Body
+# so made everything to be passed via Form only
+@router.patch("/me/updateInfo",status_code=status.HTTP_200_OK)
+def updateUserInfo(username:str=Form(None),bio:str=Form(None),profile_picture:UploadFile=File(None),db:Session=Depends(db.getDb),currentUser:sch.TokenModel=Depends(oauth2.getCurrentUser)):
+    # to store updates the user does
+    updates={}
+    if username:
+        if db.query(models.User).filter(models.User.username == username,models.User.id !=currentUser.id).first():
+            raise HTTPException(status_code=400, detail="Username already taken")
+        updates["username"] = username
+    if bio:
+        updates['bio']=bio
+    if profile_picture:
+        # creating an profilepics directory to store the
+        # users profile pics locally instead of inserting them
+        # into the db which is a bad practice
+        os.makedirs("profilepics", exist_ok=True)
+        # allowing only certain file types
+        # File.content_type method returns image/extension if its not
+        # present in our list we raise an error
+        allowedFileTypes=['image/jpeg','image/png','image/gif']
+        if profile_picture.content_type not in allowedFileTypes:
+            raise HTTPException(status_code=400,detail="only jpeg,png,gif files allowed")
+        # if not we create a filePath to store this path in the db 
+        # instead of directly storing the image itself
+        file_path=f"profilepics/{currentUser.username}_{profile_picture.filename}"
+        # py methods to copy the argumented image in our filepath
+        with open(file_path, "wb") as buffer:
+           shutil.copyfileobj(profile_picture.file, buffer)
+        updates['profile_picture']=file_path
+        # if any updates update them
+    if updates:
+        db.query(models.User).filter(models.User.id==currentUser.id).update(updates)
+        db.commit()
+        db.refresh(currentUser)
+    return updates
+
+@router.get("/me/votedOnPosts",status_code=status.HTTP_200_OK)
+def getVotedPosts(user_id:int,db:Session=Depends(db.getDb),currentUser:models.User =Depends(oauth2.getCurrentUser)):
+    voted_posts=currentUser.voted_posts
+    return {
+                f"{currentUser.username} you have voted on posts":
+            [
+                {
+                "post title":f"{posts.title}",
+                "post id":f"{posts.id}",
+                "post owner":f"{posts.user.username}"
+            } 
+                for posts in voted_posts
+        ]
+    }
+
+@router.get("/me/voteStats",status_code=status.HTTP_200_OK)
+def voteStatus(user_id:int,db:Session=Depends(db.getDb),currentUser:models.User = Depends(oauth2.getCurrentUser)):
+    # using the func,case and quering in pass
+    summary=db.query(
+        func.count(case(models.Votes.action==True,1)).label("likes"),
+        func.count(case(models.Votes.action==False,1)).label("dislikes")
+    ).filter(models.User.id==currentUser.id).all()
+    return {
+        f"{currentUser.username} here's your vote stats":
+       { 
+           "number of liked posts":summary.likes,
+           "number of dislked posts":summary.dislikes
+       }
+}
+
+@router.get("/me/likedPosts")
+def get_liked_posts(user_id: int,db:Session = Depends(db.getDb),currentUser:models.User=Depends(oauth2.getCurrentUser)):
+    # Query liked posts
+    liked_posts = (
+        db.query(models.Post)
+        .join(models.Votes, models.Votes.post_id==models.Post.id)
+        .filter(and_(models.Votes.user_id==user_id, models.Votes.action==True))
+        .all()
+    )
+    return {
+        f"{currentUser.username} your liked posts includes":
+        [
+            {
+                "post id":posts.id,
+                "post owner":posts.user.username
+            }
+            for posts in liked_posts
+        ]
+    }
+@router.get("/me/dislikedPosts")
+def get_liked_posts(user_id: int,db:Session = Depends(db.getDb),currentUser:models.User=Depends(oauth2.getCurrentUser)):    # Query disliked posts
+    liked_posts = (
+        db.query(models.Post)
+        .join(models.Votes,models.Votes.post_id==models.Post.id)
+        .filter(and_(models.Votes.user_id==user_id, models.Votes.action==False))
+        .all()
+    )
+    return {
+        f"{currentUser.username} your disliked posts includes":
+        [
+            {
+                "post id":posts.id,
+                "post owner":posts.user.username
+            }
+            for posts in liked_posts
+        ]
+    }
+
+@router.get("/me/commented-on",status_code=status.HTTP_200_OK)
+def getCommentedPosts(user_id:int,db:Session=Depends(db.getDb),currentUser:models.User =Depends(oauth2.getCurrentUser)):
+    # get the current users all commented posts id's ignore duplicates
+    uniquePostIds=db.query(distinct(models.Comments.post_id)).filter(models.Comments.user_id==currentUser.id).all()
+    # the 'uniquePostIds' is a list of tuples where each tuple is
+    # of the form (post_id1,) (post_id2,) so we exract the first elem
+    # from each of the tuples in the list
+    post_ids = [row[0] for row in uniquePostIds]
+    # query for the post_ids in the Posts table
+    commented_posts=(
+        db.query(models.Post)
+        .filter(models.Post.id.in_(post_ids))
+        .all()
+    )
+    return {
+                f"{currentUser.username} you have commented on posts":
+            [
+                {
+                "post title":f"{posts.title}",
+                "post id":f"{posts.id}",
+                "post owner":f"{posts.user.username}"
+            } 
+                for posts in commented_posts
+        ]
+    }
+
+@router.get("/me/comment-stats",status_code=status.HTTP_200_OK)
+def commentStatus(user_id:int,db:Session=Depends(db.getDb),currentUser:models.User = Depends(oauth2.getCurrentUser)):
+    comment_count = currentUser.total_comments
+    uniquePostIds=db.query(distinct(models.Comments.post_id)).filter(models.Comments.user_id==currentUser.id).count()
+    return {
+        f"{currentUser.username} here's your comment stats":
+         f"you have a total of {len(comment_count)} comment's on {uniquePostIds} post's"
+       }

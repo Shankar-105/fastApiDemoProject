20b4dbedea4317a4b7a7d7f14aa5db0f219e9ad2
Shankar-105 <shankarbhavani862@gmail.com>
2025-11-01 18:29:33 +0530

    now you can see other users profile pics

--- app/routes/users.py
+++ app/routes/users.py
@@ -4,7 +4,7 @@ import app.schemas as sch
 from app import models,db,oauth2
 from sqlalchemy.orm import Session
 import app.utils as utils
-
+import os
 router=APIRouter(
     tags=['Users']
 )
@@ -25,6 +25,24 @@ def userProfile(user_id:int,db:Session=Depends(db.getDb),currentUser:models.User
     if not userProfile.profilePicture:
         userProfile.profilePicture="no profile picture"
     return userProfile
+
+@router.get("/users/{user_id}/profile/pic",status_code=status.HTTP_200_OK)
+def myProfilePicture(user_id:int,db:Session=Depends(db.getDb),currentUser:models.User=Depends(oauth2.getCurrentUser)):
+    # get the curretn users profile pic
+    user=db.query(models.User).filter(models.User.id==user_id).first()
+    profilePicturePath = user.profile_picture
+    # if he doesnt have a porfile pic return 404
+    if not profilePicturePath:
+        raise HTTPException(status_code=404, detail="No profile picture")
+    file_path=f"profilepics/{profilePicturePath}"
+    # optional check
+    if not os.path.exists(file_path):
+        raise HTTPException(status_code=404, detail="Image not found")
+    # return a FileResponse class response to view it directly
+    # as in commit hash <96bd0a3> or else return the link and you can
+    # view it in the broswer by entering the output url
+    return sch.UserProfileDisplay.displayUserProfilePic(user)
+
 @router.post("/user/signup",status_code=status.HTTP_201_CREATED,response_model=sch.UserResponse)
 def createUser(userData:sch.UserEssentials=Body(...),db:Session=Depends(db.getDb)):
     # hash the password using the bcrypt lib
